f6f45d14fae09185b9c530b0e2b53ce00bffd719
Koltanuk <pavl.ira.a@gmail.com>
2024-11-10 20:11:37 +0200

    update for deploy

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
   });
   
